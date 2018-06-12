319d3ae2b435c507461aeedff32e901a2580f22a
Lucas Milotich <lucasmilotich@imac.cpe.telecentro.net.ar>
2018-06-11 21:38:42 -0300

    form

--- frontend/src/Components/EditButtonForm/EditButtonForm.jsx
+++ frontend/src/Components/EditButtonForm/EditButtonForm.jsx
@@ -1,63 +1,65 @@
 import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class EditButtonForm extends Component {
 
-    /*buttonTitle
-buttonQuantity
-buttonPrice
-productCode
-productName
-buttonLink*/
+
 componentDidUpdate(prevProps){
 
     
 
 }
-    render() {
-        <div>
-            <TextField
-                id="name"
-                label="Name"
-                
-                value={this.props.button.buttonTitle}
-                onChange={this.handleChange('name')}
-                margin="normal"
-            />
-            <TextField
-                id="name"
-                label="Name"
-                
-                value={this.props.button.buttonQuantity}
-                onChange={this.handleChange('name')}
-                margin="normal"
-            />
-            <TextField
-                id="name"
-                label="Name"
-                
-                value={this.props.button.productCode}
-                onChange={this.handleChange('name')}
-                margin="normal"
-            />
-            <TextField
-                id="name"
-                label="Name"
-                
-                value={this.props.button.productName}
-                onChange={this.handleChange('name')}
-                margin="normal"
-            />
-            <TextField
-                id="name"
-                label="Name"
-                
-                value={this.props.button.buttonLink}
-                onChange={this.handleChange('name')}
-                margin="normal"
-            />
-         
-        </div>
-    }
+handleChange(data){
 
 }
+    render() {
+        return (
+        <div>
+            <TextField
+                id="buttonTitle"
+                label="Titulo de botón"
+                
+                value={this.props.button != null ? this.props.button.buttonTitle : null }
+                onChange={this.handleChange('name')}
+                margin="normal"
+            />
+            <TextField
+                id="buttonQuantity"
+                label="Cantidad"
+                value={this.props.button != null ? this.props.button.buttonQuantity : null }
+                
+                onChange={this.handleChange('name')}
+                margin="normal"
+            />
+            <TextField
+                id="productCode"
+                label="Código de producto"
+                value={this.props.button != null ? this.props.button.productCode : null }
+                
+                onChange={this.handleChange('name')}
+                margin="normal"
+            />
+            <TextField
+                id="productName"
+                label="Nombre producto"
+                value={this.props.button != null ? this.props.button.productName : null }
+                
+                onChange={this.handleChange('name')}
+                margin="normal"
+            />
+            <TextField
+                id="buttonLink"
+                label="Link de botón"
+                value={this.props.button != null ? this.props.button.buttonLink : null }
+                
+                onChange={this.handleChange('name')}
+                margin="normal"
+            />
+               <Button >Guardar</Button>
+
+        </div>
+    )}
+
+}
+export default EditButtonForm;
